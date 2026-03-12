bc7b44df4f349353292b24b6b80ec201be73beb0
Clark <clark@kali.Kail>
2026-03-12 14:04:00 +0000

    feat: update wallet currency toggle

--- src/app/pages/wallet/wallet.page.ts
+++ src/app/pages/wallet/wallet.page.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { AsyncPipe, CommonModule, DatePipe, NgForOf, TitleCasePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonToggle } from '@ionic/angular/standalone';
 import { ArenaService } from '../../core/services/arena.service';
 import { BottomNavComponent } from '../../shared/bottom-nav.component';
 
 @Component({
   selector: 'app-wallet',
   standalone: true,
-  imports: [CommonModule, IonContent, AsyncPipe, NgForOf, TitleCasePipe, DatePipe, RouterLink, BottomNavComponent],
+  imports: [CommonModule, IonContent, IonToggle, AsyncPipe, NgForOf, TitleCasePipe, DatePipe, RouterLink, BottomNavComponent],
   templateUrl: './wallet.page.html',
   styleUrls: ['./wallet.page.scss'],
 })
 export class WalletPage {
   user$ = this.arena.currentUser$;
   transactions$ = this.arena.transactions$;
-  nairaBalance = 10500;
-  usdBalance = 25;
+  nairaBalance = 0;
+  usdBalance = 0;
   preferredCurrency: 'NGN' | 'USD' = this.loadPreferredCurrency();
 
   constructor(private arena: ArenaService) {}
