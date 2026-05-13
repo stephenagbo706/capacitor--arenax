e5894415c8022465e43a71cb751a65ffd235ee16
Clark <clark@kali.Kail>
2026-05-13 10:00:54 +0000

    Update login and register page UI

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -15,6 +15,7 @@ import { AuthService } from '../../../core/services/auth.service';
 export class LoginPage {
   email = '';
   password = '';
+  showPassword = false;
   error = '';
 
   constructor(private auth: AuthService, private router: Router) {}
